1662241a1669318f49e160affd933753208e64d7
Connor Hengstler <connorwh@student.cs.ubc.ca>
2025-09-20 13:58:13 -0700

    package and lint cleanup

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -4,18 +4,12 @@ module.exports = function override(config) {
     crypto: require.resolve("crypto-browserify"),
     stream: require.resolve("stream-browserify"),
     vm: require.resolve("vm-browserify"),
-    process: require.resolve("process/browser"),
     os: require.resolve("os-browserify/browser"),
   };
 
   // Add process polyfill
   config.plugins = config.plugins || [];
   const webpack = require("webpack");
-  config.plugins.push(
-    new webpack.ProvidePlugin({
-      process: "process/browser",
-    })
-  );
 
   // Suppress source map warnings
   config.ignoreWarnings = [
